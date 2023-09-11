6ea2d7d0687890a381d4c69718537ea31c0aa367
simrank321 <144449932+simrank321@users.noreply.github.com>
2023-09-11 17:25:45 +0530

    Update Assignment.java
    - Added Class & main method
    - Added basic structure with dummy variables and methods

--- Assignment.java
+++ Assignment.java
@@ -7,3 +7,32 @@
  * @ID (24297718)
  * @version (v1.0)
  */
+
+public class Assignment {
+
+    /* Declaring assignmentName as static global to make it available throughout the class*/
+    public static String assignmentName;
+
+    /**Defining main method of the program
+     * local variable of type double[]
+     */
+    public static void main(String[] args) {
+
+        // Creating an array of double data type to store marks of the Students
+        double[] marks = new double[30];
+
+        // Calling a static method to input marks of the Student 
+        inputData(marks);
+
+        // Displaying the name of the Assignment as well the marks of Student once input is done. 
+        System.out.println("The marks of student in Assignment named -- " + assignmentName + " are as follows :");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+
+        // Displaying a Saparation
+        System.out.println("--------------------------");
+
+    }
+
+}
