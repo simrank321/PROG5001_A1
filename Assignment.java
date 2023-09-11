df71a59ab5303025096b50efa63f89a74df027e7
simrank321 <144449932+simrank321@users.noreply.github.com>
2023-09-11 17:16:52 +0530

    Create Assignment.java
    created the main java file of the program.

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+/**
+ * A Java Program to take input marks obtained by students and calculate
+ * Lowest Marks
+ * Highest Marks
+ * and Standard Deviation of the marks
+ * @author (Simran Kaur)
+ * @ID (24297718)
+ * @version (v1.0)
+ */
