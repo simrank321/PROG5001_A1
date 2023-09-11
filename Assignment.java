5b7eda3feab621bdffcc0bb86dbf2b70ea863f7c
simrank321 <144449932+simrank321@users.noreply.github.com>
2023-09-11 17:50:14 +0530

    Added necessary print statements and dummy validation method
    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -45,15 +45,28 @@ public class Assignment {
 
         // Instantiating Scanner Class from the java.util package to receive input from a user at the runtime 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter title of the Assignment");
         // to accept String input from the user, it refers to the Static Global Variable hence definition of variable is not required 
         assignmentName = sc.next();
+        System.out.println("Now Please Enter the Marks of the Students");
         for (int index = 0; index < marks.length; index++) {
             // Refering to the respective Student whose  marks is to be entered.
             System.out.println("Enter marks of Student no. :" + (index + 1));
             // Defining a local variable to store the user input to first validate then assign to the actual index
             double number = 0.0;
-            // Assigning the user value to the local variable
-            number = sc.nextDouble();
+            // A flag boolean to evaluate whether the input is correct or not
+            boolean check = false;
+            /** Defining a do-while loop to first input a value from the user, 
+            check whether the input is valid, if not then redirecting to the 
+            loop unless the desired value is entered */
+            do {
+                // Assigning the user value to the local variable
+                number = sc.nextDouble();
+                // calling the checkValidity method with the input value to check if its correct or not, if correct returns true, else returns false
+                check = checkValidity(number);
+                // repeating the loop until the correct value is entered            
+            } while (check != true);
+            // once input is validated, assigning it to the respective index
             marks[index] = number;
 
         }
